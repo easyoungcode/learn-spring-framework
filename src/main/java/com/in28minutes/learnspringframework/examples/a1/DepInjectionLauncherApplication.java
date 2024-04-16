d757d80e6c47ac722d37bd2a4b1cdb025830aaa0
easyoungcode <gchun910@gmail.com>
2024-04-16 14:45:50 +0900

    생성자, Setter, 필드주입 연습

--- src/main/java/com/in28minutes/learnspringframework/examples/a1/DepInjectionLauncherApplication.java
+++ src/main/java/com/in28minutes/learnspringframework/examples/a1/DepInjectionLauncherApplication.java
@@ -15,10 +15,19 @@ class YourBusinessClass {
     Dependency1 dependency1;
     Dependency2 dependency2;
 
+    // 생성자 주입
+    public YourBusinessClass(Dependency1 dependency1, Dependency2 dependency2) {
+        super();
+        System.out.println("Constructing YourBusinessClass");
+        this.dependency1 = dependency1;
+        this.dependency2 = dependency2;
+    }
+
     public String toString() {
         return "Using "+ dependency1+" and "+dependency2;
     }
 
+/*  Setter 주입  
     @Autowired
     public void setDependency1(Dependency1 dependency1) {
         System.out.println("Setting dependency1 to "+dependency1);
@@ -28,7 +37,7 @@ class YourBusinessClass {
     public void setDependency2(Dependency2 dependency2) {
         System.out.println("Setting dependency2 to "+dependency2);
         this.dependency2 = dependency2;
-    }
+    }*/
 }
 @Component
 class Dependency1 {
