a6b6e3fc9953ee1dca6406b52677220acdadc380
easyoungcode <gchun910@gmail.com>
2024-04-16 10:06:22 +0900

    Spring Bean 생성 및 관리, Bean 호출

--- src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
+++ src/main/java/com/in28minutes/learnspringframework/App02HelloWorldSpring.java
@@ -14,11 +14,22 @@ public class App02HelloWorldSpring {
         var context =
                 new AnnotationConfigApplicationContext(HelloWorldConfiguration.class);
 
-        //HelloWorldConfiguration - @ Configuration
+        //HelloWorldConfiguration - @Configuration
         //name - @Bean
         //Spring이 관리 중인 Bean 검색
-        
+
         System.out.println(context.getBean("name"));    // Bean은 context.getBean을 사용하고 Bean의 이름을 부여하여 검색 가능
+        System.out.println(context.getBean("age"));
+        System.out.println(context.getBean("person"));
+        System.out.println(context.getBean("person2MethodCall"));
+        System.out.println(context.getBean("person3Parameters"));
+
+        // No bean named 'BeanName' available 오류 : Bean 이름과 맞지 않아 발생하는 오류
+        System.out.println(context.getBean("address2"));
+        /** Spring이 Bean을 관리하기 시작하면 Bean의 이름을 사용하거나 Bean의 유형을 사용해 가져올 수 있다 */
+//        System.out.println(context.getBean(Address.class));     // 이름이 아닌 클래스의 이름으로도 가능
+
+
     }
 
 }
--- src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
+++ src/main/java/com/in28minutes/learnspringframework/HelloWorldConfiguration.java
@@ -3,11 +3,47 @@ package com.in28minutes.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+/** record : JDK16부터 추가된 기능, record를 사용하면 생성자, getter, setter 걱정을 없애준다.
+ *  getter, setter, 생성자를 입력하지 않아도 사용 가능 */
+record Person(String name, int age, Address address) {};
+record Address(String firstLine, String city) {};
+
 @Configuration
 public class HelloWorldConfiguration {
+    /** Bean의 기본이름 = 메서드의 이름 */
 
     @Bean
     public String name() {
         return "Hello World";
+    }
+
+    @Bean
+    public int age() {
+        return 100;
+    }
+
+    @Bean
+    public Person person() {
+        return new Person("John Doe", 18, new Address("Some Street", "Some City"));
+    }
+
+    @Bean
+    public Person person2MethodCall() {
+        return new Person(name(), age(), address());
+    }
+
+    // 메서드를 호출하는 대신 매개변수를 추가하는 형태 (Spring이 관리하는 Bean 가져오는 방법 중 1개)
+    @Bean
+    public Person person3Parameters(String name, int age, Address address3) { // name, age, address2
+        return new Person(name, age, address3);
+    }
+
+    @Bean(name = "address2")
+    public Address address() {
+        return new Address("Baker Street", "London");
+    }
+    @Bean(name = "address3")
+    public Address address3() {
+        return new Address("Montana", "Hyderaba");
     }
 }
